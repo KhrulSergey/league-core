74b07ee2dd93ebd568c0a32a43596400fee5d066
Sergey Khrul <siberianodis@gmail.com>
2021-04-09 05:31:00 +0200

    Добавить вывод ParticipantDTO в Инвайт-Реквест 1. Fix параметров TeamInviteRequestDto для вывода вложенных данных http://joxi.ru/eAOXaa3Ck8wv1r

--- src/main/java/com/freetonleague/core/mapper/TeamInviteRequestMapper.java
+++ src/main/java/com/freetonleague/core/mapper/TeamInviteRequestMapper.java
@@ -10,14 +10,13 @@ import org.mapstruct.ReportingPolicy;
 import java.util.List;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR,
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+        unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = TeamParticipantMapper.class)
 public interface TeamInviteRequestMapper {
 
     @Mapping(target = "invitedUser",
             expression = "java(java.util.Objects.nonNull(entity.getInvitedUser())?entity.getInvitedUser().getLeagueId().toString() : null)")
     @Mapping(target = "teamId", source = "entity.team.id")
     @Mapping(target = "participantCreatorId", source = "entity.participantCreator.id")
-    @Mapping(target = "participantAppliedId", source = "entity.participantApplied.id")
     TeamInviteRequestDto toDto(TeamInviteRequest entity);
 
     List<TeamInviteRequestDto> toDto(List<TeamInviteRequest> entities);
