def5a13246ffc7e6291752b1bad6eee048420942
Sergey Khrul <siberianodis@gmail.com>
2021-04-30 01:09:31 +0200

    Внедрить ролевую модель для User ( regular и admin) - https://freetonleague.monday.com/boards/1109042794/pulses/1223169997 1. Создана модель Role.java и список возможных ролей в UserRoleType и список ролей пользователя через доп. связку User.roles. 2. Создана необходимая миграция данных V024 3. Создан репозиторий и дорботан сервис создания пользователей с проставлением роли Regular

--- src/main/java/com/freetonleague/core/domain/enums/UserStatusType.java
+++ src/main/java/com/freetonleague/core/domain/enums/UserStatusType.java
@@ -1,10 +1,17 @@
 package com.freetonleague.core.domain.enums;
 
+import java.util.List;
+
 public enum UserStatusType {
     ACTIVE,
     CREATED,
     BANNED,
     DELETED;
+
+    public static List<UserStatusType> activeUserStatusList = List.of(
+            ACTIVE,
+            CREATED
+    );
 
     public boolean isCreated() {
         return this == CREATED;
