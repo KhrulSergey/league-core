eb612e3db66d3f716f7e94a33502808a24f7640f
Sergey Khrul <siberianodis@gmail.com>
2021-05-01 21:15:58 +0200

    Доработать обмен данных с микросервисом Broxus - https://freetonleague.monday.com/boards/1109042794/pulses/1257750716 1. Временно восстановлена проверка access_token на уровне REST запроса 2. Minor fix в миграциях для безошибочного обновления данных

--- src/main/java/com/freetonleague/core/service/financeUnit/FinancialUnitController.java
+++ src/main/java/com/freetonleague/core/service/financeUnit/FinancialUnitController.java
@@ -2,15 +2,12 @@ package com.freetonleague.core.service.financeUnit;
 
 import com.freetonleague.core.domain.dto.AccountDepositFinUnitDto;
 import com.freetonleague.core.domain.enums.BankProviderType;
-import com.freetonleague.core.domain.model.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping(path = FinancialUnitController.BASE_PATH)
@@ -26,9 +23,8 @@ public class FinancialUnitController {
     @ApiOperation("Callback method for deposit to user accounts")
     @PostMapping(path = PATH_DEPOSIT)
     public ResponseEntity<Void> getDisciplineById(@RequestParam(value = "access_token", required = true) String token,
-                                                  @RequestBody AccountDepositFinUnitDto accountDepositInfo,
-                                                  @ApiIgnore @AuthenticationPrincipal User user) {
-        restFinancialUnitFacade.processDeposit(accountDepositInfo, BankProviderType.BROXUS, user);
+                                                  @RequestBody AccountDepositFinUnitDto accountDepositInfo) {
+        restFinancialUnitFacade.processDeposit(token, accountDepositInfo, BankProviderType.BROXUS);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
