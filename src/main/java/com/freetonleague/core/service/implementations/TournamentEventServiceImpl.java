09488b42d090f67501489f281b7ca8fddeae0129
Sergey Khrul <siberianodis@gmail.com>
2021-07-03 23:22:15 +0200

    Добавить возможность возврата вступительного взноса в случае отмены регистрации участником на турнир - https://freetonleague.monday.com/boards/1109042794/pulses/1445468333 1. Fix параметры методов управления заявками на участие в турнире 2. Fix зависимостей-библиотек

--- src/main/java/com/freetonleague/core/service/implementations/TournamentEventServiceImpl.java
+++ src/main/java/com/freetonleague/core/service/implementations/TournamentEventServiceImpl.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
