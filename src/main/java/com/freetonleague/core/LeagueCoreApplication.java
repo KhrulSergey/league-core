3dc097ed4ddd40484bc446241c4e4a18467d32a7
Sergey Khrul <siberianodis@gmail.com>
2021-07-26 22:02:40 +0200

    Подключить KUNA провайдер валютной информации - https://freetonleague.monday.com/boards/1109042794/pulses/1509659509 1. Fix scheduling

--- src/main/java/com/freetonleague/core/LeagueCoreApplication.java
+++ src/main/java/com/freetonleague/core/LeagueCoreApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
