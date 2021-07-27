e06e11d65c1afdd23bfd976c21ae678ac1854b68
Константин Маяковский <mayakplay@gmail.com>
2021-07-27 23:20:40 +0300

    Доработки по рулетке

--- src/main/java/com/freetonleague/core/repository/RouletteBetRepository.java
+++ src/main/java/com/freetonleague/core/repository/RouletteBetRepository.java
@@ -3,6 +3,7 @@ package com.freetonleague.core.repository;
 import com.freetonleague.core.domain.entity.RouletteBetEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -13,6 +14,6 @@ public interface RouletteBetRepository extends JpaRepository<RouletteBetEntity, 
     Long sumForAllTime();
 
     @Query("SELECT sum(e.tonAmount) from RouletteBetEntity e where e.createdAt < :end and e.createdAt > :start")
-    Long sumForPeriod(LocalDateTime start, LocalDateTime end);
+    Long sumForPeriod(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
 }
