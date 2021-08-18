289233cc94144eafe0a218646e8a6a662accbb02
Константин Маяковский <mayakplay@gmail.com>
2021-08-18 23:58:44 +0300

    Доработки по рулетке

--- src/main/java/com/freetonleague/core/controller/api/RouletteApi.java
+++ src/main/java/com/freetonleague/core/controller/api/RouletteApi.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,7 +42,7 @@ public interface RouletteApi {
             @PageableDefault Pageable pageable
     );
 
-    @GetMapping(POST_MAKE_BET_PATH)
+    @PostMapping(POST_MAKE_BET_PATH)
     @ApiOperation("Make bet by user")
     RouletteStatsDto makeBet(
             @RequestBody @Valid RouletteBetFilter filter,
